948b07e00eabfe8d495ee10a1ca7e949f7b8c251
Alan <74235604+Elsefai@users.noreply.github.com>
2026-09-23 11:40:35 +0900

    Add files via upload

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+    int x;
+    int y;
+
+    scanf("%d %d",&x,&y);
+
+    printf("SUM: %d\n",x+y);
+    printf("Difference: %d\n",x-y);
+    printf("Product: %d\n",x*y);
+    printf("Quotient: %d\n",x/y);
+    return 0;
+}
